11041d6c20117b499d7a53e294b2fd66b21df2f0
Aljoscha Wiesner <aljo0025@easv365.dk>
2021-04-04 20:39:17 +0200

    added some routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import {store} from '../store'
 import {ConnectedDashboard} from "./Dashboard";
+import {Route} from 'react-router-dom';
+import { ConnectedNavigation } from "./Navigation"
+import {history} from '../store/history';
+import {Router} from "react-router-dom";
 
+//This will be our main Component from where we work.
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-          <ConnectedDashboard/>
-        </div>
-    </Provider>
+   <Router history={history}>
+       <Provider store={store}>
+           <div>
+               <ConnectedNavigation/>
+               <Route exact path='/dashboard'
+                      render={() => (<ConnectedDashboard/>)}
+                      />
+           </div>
+       </Provider>
+   </Router>
 )
